d274a9d938c787d862d1e6696c7868a128a69e7d
Ivan Kurban <ivankurban1986@gmail.com>
2022-01-13 23:02:32 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+##Apach server log parser
+___
+
+Web interface is created in Flask and Bootstrap 4.
+
+___
+Why is it necessary?
+For example, if someone ddos (DDoS) your site or client's site. Or, if someone is trying to brute force (sort out) the password for the site admin panel, and you need to find out the IP address of the asshole and ban him in .htaccess (not the search bot, of course).
+___
---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+import re
+from collections import Counter
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        f = request.files['file'].read()
+        txt = str(f.decode('utf-8'))
+
+        pattern = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+        ips = re.findall(pattern, txt)
+
+        result = Counter(ips).most_common(10)
+
+        ban = []
+        for key, value in result:
+            if value > 100:
+                ban.append({'ip': key, 'frequency': value})
+
+        return render_template('index.html', ips=ban)
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
